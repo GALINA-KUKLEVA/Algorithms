f3edf641b8dd675a6b0718ad712cff57834b204e
Galina <gkukleva734@gmail.com>
2021-10-10 23:20:52 -0600

    Code reformated

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 
 public class FizzBuzz {
 
-    public String[] fizzBuzz(int startNumber, int endNumber){
+    public String[] fizzBuzz(int startNumber, int endNumber) {
         String[] numbers = new String[endNumber - startNumber + 1];
         int newNumber = startNumber;
 
-        for (int i = startNumber - startNumber; i < (endNumber - startNumber + 1); i++){
+        for (int i = startNumber - startNumber; i < (endNumber - startNumber + 1); i++) {
             if (newNumber % 15 == 0) {
                 numbers[i] = "FizzBuzz";
-            } else if (newNumber % 3 == 0){
+            } else if (newNumber % 3 == 0) {
                 numbers[i] = "Fizz";
-            } else if (newNumber % 5 == 0){
+            } else if (newNumber % 5 == 0) {
                 numbers[i] = "Buzz";
             } else {
                 numbers[i] = String.valueOf(newNumber);
             }
-            newNumber +=1;
+            newNumber += 1;
         }
         System.out.println(Arrays.toString(numbers));
         return numbers;
